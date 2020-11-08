61e4273defb0b4faa802735b09b745aa1411cbb2
mikebrucato <brucatomike1@gmail.com>
2020-11-07 18:51:11 -0600

    added html element variables

---
+++ script.js
@@ -0,0 +1,14 @@
+// list of element variables including open weather api key
+var apiKey = "6d2d9229c4237c0213d63e723601dc2c";
+var searchBtn = $(".searchBtn");
+var searchInput = $(".searchInput");
+var cityNameEl = $(".city");
+var currentDateEl = $(".currentDate");
+var weatherIconEl = $(".weatherIcon");
+var searchHistoryEl = $(".historyItems");
+var tempEl = $(".temp");
+var humidityEl = $(".humidity");
+var windSpeedEl = $(".windSpeed");
+var uvIndexEl = $(".uvIndex");
+var cardRow = $(".card-row");
+
